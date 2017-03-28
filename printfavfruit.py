4d6fd5d8c3cd1f32d12e6131bb7fc1bf48c186e5
joykim137 <joykim@seoulchristian.org>
2017-03-28 19:19:41 +0900

    Add files via upload

---
+++ printfavfruit.py
@@ -0,0 +1,6 @@
+# prints "apples" and prints "oranges" on a new line
+print "apples\noranges"
+# prints "pineapples" and print "pears" on a new line
+print "pineapples\npears"
+# prints strawberries
+print "strawberries"
